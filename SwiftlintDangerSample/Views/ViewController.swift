d4adb58ffb76036a3b54e5aa34253f1cb71a23f5
Didik Maulana Ardiansyah <didik@mamiteam.com>
2021-10-19 14:52:13 +0700

    refactor(project): separated and grouping files

---
+++ SwiftlintDangerSample/Views/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SwiftlintDangerSample
+//
+//  Created by Didik on 18/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
