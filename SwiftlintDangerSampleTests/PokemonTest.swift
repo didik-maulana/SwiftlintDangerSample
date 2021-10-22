8f7aaca67fba25681a3ea3f8a28c4d0acae2f179
Didik Maulana Ardiansyah <didik@mamiteam.com>
2021-10-22 10:19:43 +0700

    unittest:(model): Created Pokemon Model Test

---
+++ SwiftlintDangerSampleTests/PokemonTest.swift
@@ -0,0 +1,38 @@
+//
+//  PokemonTest.swift
+//  SwiftlintDangerSampleTests
+//
+//  Created by Didik on 22/10/21.
+//
+
+import XCTest
+@testable import SwiftlintDangerSample
+
+class PokemonTest: XCTestCase {
+  
+  private let pokemonName: String = "Sylveon"
+  private let pokemonType: PokemonType = .attacker
+  private let pokemonImageName: String = "sylveon"
+  private let pokemonSkills: String = "Attacker, Ranged"
+  private var pokemon: Pokemon? = nil
+  
+  override func setUpWithError() throws {
+    pokemon = Pokemon(
+      name: pokemonName,
+      type: pokemonType,
+      imageName: pokemonImageName,
+      skills: pokemonSkills
+    )
+  }
+  
+  override func tearDownWithError() throws {
+    pokemon = nil
+  }
+  
+  func testPokemonShouldBeCorrectValue() throws {
+    XCTAssertEqual(pokemon?.name, pokemonName)
+    XCTAssertEqual(pokemon?.type, pokemonType)
+    XCTAssertEqual(pokemon?.imageName, pokemonImageName)
+    XCTAssertEqual(pokemon?.skills, pokemonSkills)
+  }
+}
