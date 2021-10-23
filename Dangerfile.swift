a8418a793a6c706e9752856cda52b78f1dc1e829
Didik Maulana <didikmaulana49@gmail.com>
2021-10-23 15:54:47 +0700

    update

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,15 @@
-import Danger 
+import Danger
+
 let danger = Danger()
 
-let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-message("These files have changed: \(editedFiles.joined(separator: ", "))")
+// Encourage changelog entries
+let hasChangelog = danger.git.modifiedFiles.contains("CHANGELOG.md")
+if (!hasChangelog) {
+    warn("Please add a CHANGELOG.md entry for your changes.")
+}
+
+// Encourage smaller PRs
+var bigPRThreshold = 1000;
+if (danger.github.pullRequest.additions + danger.github.pullRequest.deletions > bigPRThreshold) {
+  warn("Pull Request size seems relatively large. If this Pull Request contains more changes, please split each into separate PR will helps faster, easier review.")
+}
